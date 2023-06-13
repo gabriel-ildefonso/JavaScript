016ae4ef631521f783b3f75ec10f2176b237e49c
unknown <ssgabrielvinicius@gmail.com>
2023-06-13 10:40:10 -0300

    arrays

---
+++ 20 - Array/array.js
@@ -0,0 +1,11 @@
+const shoppingList = []
+console.log(Purlist)
+
+shoppingList[0] = 'bread'
+shoppingList[1] = 'eggs'
+shoppingList[2] = 45
+shoppingList[5] = 'fish'
+//You can determine an array element later in the list, and by doing so, the spaces before it that have not yet been instantiated will be empty.
+
+console.log(shoppingList) //prints the entire array  ['bread', 'eggs', 45, 'fish']
+console.log(shoppingList[2]) //prints the array item referenced by the index    [45]
