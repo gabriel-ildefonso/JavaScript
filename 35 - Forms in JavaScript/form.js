9c9fa7b789f93bcd90e2dd2fd0928bc332d89e43
gabriel-ildefonso <101571743+gabriel-ildefonso@users.noreply.github.com>
2023-08-30 08:34:53 -0300

    forms in javascript

---
+++ 35 - Forms in JavaScript/form.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("order-form");
+
+form.addEventListener("submit", function (ev) {
+  const name = document.querySelector('input[name="name"]').value;
+  const adress = document.querySelector('input[name="adress"]'.value);
+  const breadType = document.querySelector('select[name="breadType"]').value;
+  const main = document.querySelector('input[name="main"]').value;
+});
