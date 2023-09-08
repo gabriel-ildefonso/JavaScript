1aa92264aa3aa70ec3b00493d4d7ee0f889e22a0
gabriel-ildefonso <101571743+gabriel-ildefonso@users.noreply.github.com>
2023-09-07 22:56:54 -0300

    Null coalescence operator

---
+++ IV - Modern JavaScript/51 - Null coalescence operator/nullCoalescence.js
@@ -0,0 +1,21 @@
+const a = 0
+
+const b = null
+
+const c = 'Test'
+
+console.log(a || b || c)
+//Retornará 'Test', pois 0 é falso, e null também é falso
+
+console.log(a ?? b ?? c)
+//retornará 0, pois 0 não é null e nem undefined
+
+let person = {
+  name: "John Doe",
+  email: "doejohn@email.com",
+  birthay: "1990-03-14"
+  age: 32
+  }
+  
+  let x = 0
+  x = person.data?.age
