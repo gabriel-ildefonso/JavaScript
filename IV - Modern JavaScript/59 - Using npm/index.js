63ecb2c8e38c9cad731fe706e77002870251bb83
gabriel-ildefonso <101571743+gabriel-ildefonso@users.noreply.github.com>
2023-09-08 10:34:54 -0300

    using npm

---
+++ IV - Modern JavaScript/59 - Using npm/index.js
@@ -0,0 +1,6 @@
+const lodash = require('lodash')
+
+//para testar, vamos usar o método do lodash isArray, que verifica se o parâmetro passado é um array
+console.log(lodash.isArray([]))
+//kebabCase reescreve uma string no formato kebabCase
+console.log(lodash.kebabCase('Gabriel Silveira'))
